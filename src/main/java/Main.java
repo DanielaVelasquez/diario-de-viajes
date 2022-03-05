7fea635b2053644f5191b3af1981a2feeb095f07
dvelasquez <dvelasquez@prontoforms.com>
2022-03-05 15:43:12 -0500

    Segunda refactorizacion de la UI

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import ui.DiarioViajesUI;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DiarioViajesUI ui = new DiarioViajesUI();
+		ui.leerYCrearViaje();
+
+	}
+
+}
--- src/main/java/ui/DiarioViajesUI.java
+++ src/main/java/ui/DiarioViajesUI.java
@@ -9,54 +9,60 @@ import modelo.Viajero;
 
 public class DiarioViajesUI {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		Destino destino = crearDestino(scanner);
-		Viajero viajero = crearViajero(scanner);
-		Instant fechaViaje = leerFecha(scanner);
+	private Scanner scanner;
+
+	public DiarioViajesUI() {
+		scanner = new Scanner(System.in);
+	}
+
+	public Viaje leerYCrearViaje() {
+		Destino destino = crearDestino();
+		Viajero viajero = crearViajero();
+		Instant fechaViaje = leerFecha();
 		Viaje viaje = new Viaje(fechaViaje, destino, viajero);
 
 		System.out.println("Viaje creado");
+		return viaje;
 	}
 
-	private static Instant leerFecha(Scanner scanner) {
-		int anio = leerInt(scanner, "Ingrese el año del viaje");
-		int mes = leerInt(scanner, "Ingrese el mes del viaje");
-		int dia = leerInt(scanner, "Ingrese el dia del viaje");
+	private Instant leerFecha() {
+		int anio = leerInt("Ingrese el año del viaje");
+		int mes = leerInt("Ingrese el mes del viaje");
+		int dia = leerInt("Ingrese el dia del viaje");
 
-		String fechaViajeString = anio + "-" + mes + "-" + dia+"T00:00:00.00Z";
+		String fechaViajeString = anio + "-" + mes + "-" + dia + "T00:00:00.00Z";
 
 		Instant fechaViaje = Instant.parse(fechaViajeString);
 		return fechaViaje;
 	}
 
-	private static Viajero crearViajero(Scanner scanner) {
-		String nombreViajero = leerString(scanner, "Ingrese nombre del viajero");
-		int edadViajero = leerInt(scanner, "Ingrese edad del viajero");
+	private Viajero crearViajero() {
+		String nombreViajero = leerString("Ingrese nombre del viajero");
+		int edadViajero = leerInt("Ingrese edad del viajero");
 		Viajero viajero = new Viajero(nombreViajero, edadViajero);
 		return viajero;
 	}
 
-	private static Destino crearDestino(Scanner scanner) {
-		String nombreDestino = leerString(scanner, "Ingrese nombre del destino");
-		double latitudDestino = leerDouble(scanner, "Ingrese latitud del destino");
-		double longitudDestino = leerDouble(scanner, "Ingrese longitud del destino");
+	private Destino crearDestino() {
+		String nombreDestino = leerString("Ingrese nombre del destino");
+		double latitudDestino = leerDouble("Ingrese latitud del destino");
+		double longitudDestino = leerDouble("Ingrese longitud del destino");
 
 		Destino destino = new Destino(nombreDestino, latitudDestino, longitudDestino);
 		return destino;
 	}
 
-	private static int leerInt(Scanner scanner, String s) {
-		System.out.println(s);
+	private int leerInt(String mensaje) {
+		System.out.println(mensaje);
 		return scanner.nextInt();
 	}
 
-	private static double leerDouble(Scanner scanner, String mensaje) {
+	private double leerDouble(String mensaje) {
 		System.out.println(mensaje);
 		return scanner.nextDouble();
 	}
 
-	private static String leerString(Scanner scanner, String mensaje) {
+	private String leerString(String mensaje) {
 		System.out.println(mensaje);
 		return scanner.next();
 	}
